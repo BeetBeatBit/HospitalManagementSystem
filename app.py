d0b68c024bf6e3e5370dd29a80eb03193c43bd29
Daniel Carbajal <36678639+BeetBeatBit@users.noreply.github.com>
2023-02-11 01:52:40 -0600

    Login, Images, new templates
    Se agrego la pantalla de login, nuevas imagenes, y se asignaron las referencias a cada elemento de la nav bar.

--- app.py
+++ app.py
@@ -5,8 +5,28 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/especialidades.html")
+def espec():
+    return render_template("especialidades.html")
+
+@app.route("/servicios.html")
+def serv():
+    return render_template("servicios.html")
+
+@app.route("/quienesSomos.html")
+def quiens():
+    return render_template("quienesSomos.html")
+
+@app.route("/contactanos.html")
+def contact():
+    return render_template("contactanos.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("/login.html")
+
 @app.route("/index.html")
-def home():
+def indexR():
     return render_template("index.html")
 
 if __name__ == "__main__":
