34e05f37b57915cb5b39d64c76926d1eb96d343e
Daniel Carbajal <36678639+BeetBeatBit@users.noreply.github.com>
2023-02-10 19:46:54 -0600

    Add image y cambios en html
    *Se agrego una imagen de prueba
    *Se hicieron cambios en el app.py para referenciar cada html en el nav bar.
    *Se hicieron cambios en los nombres del nav bar.

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/index.html")
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
