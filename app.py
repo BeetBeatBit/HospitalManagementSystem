055c0e626856e5dd99d7afd9ac13e5277495f88c
Daniel Carbajal <36678639+BeetBeatBit@users.noreply.github.com>
2023-02-27 02:10:26 -0600

    Coneccion con base de datos
    Se agrego conexion con base de datos
    Se agregaron mensajes en login confirmando que se encontro al usuario en la base y un mensaje si no.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import mysql.connector
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -28,6 +30,46 @@ def login():
 @app.route("/index.html")
 def indexR():
     return render_template("index.html")
+
+
+@app.route('/verificar', methods=['GET', 'POST'])
+def login2():
+    if request.method == 'POST':
+        # Recupera los datos ingresados por el usuario en el input
+        email = request.form['email']
+        password = request.form['contraseña']
+        # Establecer la conexión a la base de datos
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Daniel98@",
+            database="clinica"
+        )
+        # Crear una consulta SQL para seleccionar los valores de correo y contraseña desde la tabla de usuarios
+        sql = "SELECT username, password FROM users WHERE username = %s AND password = %s"
+        values = (email, password)
+        
+        # Ejecutar la consulta SQL y obtener los resultados
+        cursor = db.cursor()
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        # Comparar los valores de correo y contraseña
+        if result and result[0] == email and result[1] == password:
+            # Credenciales válidas, redirigir al usuario a la página de inicio de sesión
+            print("Si encontre")
+            
+            error = 'Credenciales válidas. Bienvenido.'
+            return render_template('/login.html', error=error)
+            
+            #return redirect(url_for('inicio'))
+        else:
+            # Credenciales no válidas, mostrar un mensaje de error
+            print("NO encontre")
+            
+            error = 'Credenciales no válidas. Intente de nuevo.'
+            return render_template('/login.html', error=error)
+    else:
+        return render_template('/login.html', error=error)
 
 if __name__ == "__main__":
     app.run(port=4000, host="0.0.0.0")
